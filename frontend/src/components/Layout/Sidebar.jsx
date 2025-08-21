eb850b9f9219f21440678805d37c7ad5db78e5c5
Ponkan2077 <reyflores12380@gmail.com>
2025-08-21 22:02:42 +0800

    Added Sibar UI Link states

--- frontend/src/components/Layout/Sidebar.jsx
+++ frontend/src/components/Layout/Sidebar.jsx
@@ -20,12 +20,12 @@ const Sidebar = ({toggleSidebar, isIconsOnly}) => {
             <div className={`position-relative ${isIconsOnly ? "iconsOnlySidebarWrapper" : ""}`}>
                 <span className= {`fw-bold fs-1 ms-md-3 ms-1 ${isIconsOnly ? "d-none": ""}`} > LOGO </span>
                 < X className="position-absolute top-0 end-0 ms-md-3 ms-1 mt-2 d-md-none " onClick={toggleSidebar} />
-                <nav className="sidebar ms-md-3 ms-1">
+                <nav className={`sidebar ${isIconsOnly ? "d-flex w-100 align-items-center" : "mx-md-3 mx-1" }`}>
                     <span className='mt-4'>Menu</span>
-                    <ul className='ms-md-2 p-md-2 ms-1 p-1'>
+                    <ul className='ms-md-2'>
                         <li>
-                            <NavLink to="/Dashboard"className='fs-6'>
-                                <LayoutDashboard className="inline-block align-text-bottom me-md-2 me-1" style={{ width: "1.4em", height: "1.4em" }}  /> 
+                            <NavLink to="/Dashboard" className='fs-6'>
+                                <LayoutDashboard className={`inline-block align-text-bottom me-md-2 me-1`} style={{ width: "1.4em", height: "1.4em" }}  /> 
                                 <span>Dashboard</span> 
                             </NavLink>
                         </li>
@@ -67,7 +67,7 @@ const Sidebar = ({toggleSidebar, isIconsOnly}) => {
                         </li>
                     </ul>
                     <span className='mt-5'>Others</span>
-                    <ul className='ms-md-2 p-md-2 ms-1 p-1'>
+                    <ul className='ms-md-2'>
                         <li>
                             <NavLink to="/UserManagement" className='fs-6' >
                                 <User className="inline-block align-text-bottom me-md-2 me-1" style={{ width: "1.4em", height: "1.4em" }}  />
